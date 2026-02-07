b55b11e1ec1b6fe7ebda8acf7e69982f50199232
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-07 19:13:56 -0300

    refactor: change the type of pk key on users

--- security/src/main/java/com/hebertzin/security_service/repository/UserRepository.java
+++ security/src/main/java/com/hebertzin/security_service/repository/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     public Optional<User> findByEmail(String email);
 
     public boolean existsByEmail(String email);
