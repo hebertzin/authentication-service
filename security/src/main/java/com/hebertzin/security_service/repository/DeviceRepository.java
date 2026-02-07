1c9b9beb24ccf6a60030b10a6cbbe95c2ffa4c6c
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-07 20:33:40 -0300

    chore: create new method on repository

--- security/src/main/java/com/hebertzin/security_service/repository/DeviceRepository.java
+++ security/src/main/java/com/hebertzin/security_service/repository/DeviceRepository.java
@@ -3,9 +3,10 @@ package com.hebertzin.security_service.repository;
 import com.hebertzin.security_service.repository.entities.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, UUID> {
+    List<Device> findByUserId(UUID userId);
 }
