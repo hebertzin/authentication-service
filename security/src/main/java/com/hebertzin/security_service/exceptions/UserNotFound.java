664d3eeae8e93f8575e812b5e89540d52ee6815b
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-07 13:10:06 -0300

    refactor: removing unnecessary interfaces

--- security/src/main/java/com/hebertzin/security_service/exceptions/UserNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.hebertzin.security_service.exceptions;
-
-public class UserNotFound extends RuntimeException {
-    public UserNotFound(String message) {
-        super(message);
-    }
-}
