5fcfd711874e19b6c9e5b8fedf560e9aa1f3a297
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-07 22:50:14 -0300

    feat: add device verification in authentication service

--- security/src/main/java/com/hebertzin/security_service/services/AuthenticationServiceImpl.java
+++ security/src/main/java/com/hebertzin/security_service/services/AuthenticationServiceImpl.java
@@ -1,9 +1,14 @@
 package com.hebertzin.security_service.services;
 import com.hebertzin.security_service.domain.AuthenticationService;
+import com.hebertzin.security_service.domain.DeviceService;
 import com.hebertzin.security_service.domain.TokenProvider;
+import com.hebertzin.security_service.exceptions.ForbiddenException;
 import com.hebertzin.security_service.exceptions.InvalidCredentialException;
 import com.hebertzin.security_service.exceptions.NotFoundException;
+import com.hebertzin.security_service.presentation.AuthenticationRequest;
+import com.hebertzin.security_service.presentation.TrustLevel;
 import com.hebertzin.security_service.repository.UserRepository;
+import com.hebertzin.security_service.repository.entities.Device;
 import com.hebertzin.security_service.repository.entities.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,32 +17,46 @@ import java.util.Optional;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
-
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
+    private  final DeviceService deviceService;
 
     public AuthenticationServiceImpl(UserRepository userRepository,
                                      PasswordEncoder passwordEncoder,
-                                     TokenProvider tokenProvider) {
+                                     TokenProvider tokenProvider,
+                                     DeviceService deviceService
+    ) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.tokenProvider = tokenProvider;
+        this.deviceService = deviceService;
     }
 
-    public String authenticate(String email, String password) {
-        Optional<User> user = this.userRepository.findByEmail(email);
+    public String authenticate(AuthenticationRequest authenticationRequest) {
+        Optional<User> user = this.userRepository.findByEmail(authenticationRequest.email());
 
-        boolean existentUser = this.userRepository.existsByEmail(email);
+        boolean existentUser = this.userRepository.existsByEmail(authenticationRequest.email());
 
         if (!existentUser) {
             throw new NotFoundException("User not found");
         }
 
-        boolean isCorrectCredentials = this.passwordEncoder.matches(password, user.get().getPassword());
+        boolean isCorrectCredentials = this.passwordEncoder.matches(authenticationRequest.password(), user.get().getPassword());
 
         if (!isCorrectCredentials) {
             throw  new InvalidCredentialException("Credential are invalids");
+        }
+
+       Device device =  this.deviceService.createOrFindDevice(user.get().getId(),
+                authenticationRequest.deviceType(),
+                authenticationRequest.platform(),
+                authenticationRequest.userAgent(),
+                authenticationRequest.ip()
+        );
+
+        if (device.getTrustLevel() == TrustLevel.UNTRUSTED) {
+             throw  new ForbiddenException("OTP required");
         }
 
          return this.tokenProvider.generateToken(user.get().getEmail());
