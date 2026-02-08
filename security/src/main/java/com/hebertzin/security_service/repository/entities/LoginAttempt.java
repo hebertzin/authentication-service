a0f6bd2cea19bfa37419a8b4288aad784af69597
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-08 00:05:08 -0300

    feat: create login attempt entity

--- security/src/main/java/com/hebertzin/security_service/repository/entities/LoginAttempt.java
+++ security/src/main/java/com/hebertzin/security_service/repository/entities/LoginAttempt.java
@@ -15,22 +15,92 @@ public class LoginAttempt {
     @Column(nullable = false, length = 255)
     private String email;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(name = "user_id")
+    private UUID userId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "device_id")
-    private Device device;
+    @Column(name = "device_id")
+    private UUID deviceId;
 
-    @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private String result;
 
     @Column(length = 45)
     private String ip;
 
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt = Instant.now();
 
+    protected LoginAttempt() {
+    }
+
+    public LoginAttempt(
+            String email,
+            UUID userId,
+            UUID deviceId,
+            String result,
+            String ip
+    ) {
+        this.email = email;
+        this.userId = userId;
+        this.deviceId = deviceId;
+        this.result = result;
+        this.ip = ip;
+        this.createdAt = Instant.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public UUID getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(UUID deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 }
