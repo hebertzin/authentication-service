a232872ade27d7d076681bdae002d05e45e8388b
hebert santos <hebertsantosdeveloper@gmail.com>
2026-02-07 21:00:23 -0300

    chore: ajust device repository

--- security/src/main/java/com/hebertzin/security_service/repository/DeviceRepository.java
+++ security/src/main/java/com/hebertzin/security_service/repository/DeviceRepository.java
@@ -4,9 +4,11 @@ import com.hebertzin.security_service.repository.entities.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, UUID> {
-    List<Device> findByUserId(UUID userId);
+    Optional<Device> findByUserIdAndFingerprint(UUID userId, String fingerprint);
+    long countByUserId(UUID userId);
 }
